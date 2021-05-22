a0e9aa8650cb07ec2eeec7f8d859ebfc39c2dcf9
Yoshiteru Kawahara <teruponn56@gmail.com>
2021-05-22 17:02:51 +0900

    Trends修正

--- lib/twitterAPI_web_view/tweet_tile.dart
+++ lib/twitterAPI_web_view/tweet_tile.dart
@@ -5,19 +5,20 @@ import 'params_enum.dart';
 
 class TweetTile extends StatelessWidget {
   TweetTile(Map<String, String> paramList)
-      : tweetId = paramList[RecentSearchParams.id.asString]!,
-        authorId = paramList[RecentSearchParams.authorId.asString]!,
-        text = paramList[RecentSearchParams.text.asString]!,
-        createdAt = paramList[RecentSearchParams.createdAt.asString]!,
-        replyCount = paramList[RecentSearchParams.replyCount.asString]!,
-        retweetCount = paramList[RecentSearchParams.retweetCount.asString]!,
-        likeCount = paramList[RecentSearchParams.likeCount.asString]!,
-        name = paramList[RecentSearchParams.name.asString]!,
-        userName = paramList[RecentSearchParams.userName.asString]!,
+      : tweetId = paramList[RecentSearchParams.id.asString] ?? 'ERROR',
+        authorId = paramList[RecentSearchParams.authorId.asString] ?? 'ERROR',
+        text = paramList[RecentSearchParams.text.asString] ?? 'null',
+        createdAt = paramList[RecentSearchParams.createdAt.asString] ?? 'ERROR',
+        replyCount =
+            paramList[RecentSearchParams.replyCount.asString] ?? 'ERROR',
+        retweetCount =
+            paramList[RecentSearchParams.retweetCount.asString] ?? 'ERROR',
+        likeCount = paramList[RecentSearchParams.likeCount.asString] ?? 'ERROR',
+        name = paramList[RecentSearchParams.name.asString] ?? 'ERROR',
+        userName = paramList[RecentSearchParams.userName.asString] ?? 'ERROR',
         profileImageUrl =
-            paramList[RecentSearchParams.profileImageUrl.asString]! {
-    print('profileImageUrl:$profileImageUrl');
-  }
+            paramList[RecentSearchParams.profileImageUrl.asString] ??
+                'https://p.e-words.jp/img/Null.jpg';
 
   final String tweetId;
   final String authorId;
@@ -66,12 +67,13 @@ class TweetTile extends StatelessWidget {
             child: Column(
               children: [
                 SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.9,
+                  /* width: MediaQuery.of(context).size.width * 0.9, */
                   child: Text(
                     text,
                     textAlign: TextAlign.left,
                   ),
                 ),
+                const SizedBox(height: 5),
                 ConstrainedBox(
                   constraints: BoxConstraints(
                     maxWidth: MediaQuery.of(context).size.width * 0.9,
@@ -90,7 +92,7 @@ class TweetTile extends StatelessWidget {
                                 const Icon(Icons.ac_unit),
                                 Text(
                                   ' $name',
-                                  style: const TextStyle(fontSize: 14),
+                                  style: const TextStyle(fontSize: 12),
                                 ),
                               ],
                             ),
@@ -107,7 +109,7 @@ class TweetTile extends StatelessWidget {
                                 const Icon(Icons.terrain),
                                 Text(
                                   ' @$userName',
-                                  style: const TextStyle(fontSize: 14),
+                                  style: const TextStyle(fontSize: 12),
                                   overflow: TextOverflow.fade,
                                 ),
                               ],
@@ -118,49 +120,57 @@ class TweetTile extends StatelessWidget {
                     ),
                   ),
                 ),
+                const SizedBox(height: 5),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    SizedBox(
-                      child: GestureDetector(
-                        onTap: () {
-                          // url launcher
-                        },
-                        child: Row(
-                          children: [
-                            const Icon(Icons.messenger),
-                            Text(' $replyCount'),
-                          ],
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // url launcher
+                          },
+                          child: Row(
+                            children: [
+                              const Icon(Icons.messenger),
+                              replyCount == '0'
+                                  ? const Text(' ')
+                                  : Text(' $replyCount'),
+                            ],
+                          ),
                         ),
-                      ),
+                        GestureDetector(
+                          onTap: () {
+                            // url launcher
+                          },
+                          child: Row(
+                            children: [
+                              const Icon(Icons.loop),
+                              retweetCount == '0'
+                                  ? const Text(' ')
+                                  : Text(' $retweetCount'),
+                            ],
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // url launcher
+                          },
+                          child: Row(
+                            children: [
+                              const Icon(Icons.favorite),
+                              likeCount == '0'
+                                  ? const Text(' ')
+                                  : Text(' $likeCount'),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                     SizedBox(
-                      child: GestureDetector(
-                        onTap: () {
-                          // url launcher
-                        },
-                        child: Row(
-                          children: [
-                            const Icon(Icons.loop),
-                            Text(' $retweetCount'),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      child: GestureDetector(
-                        onTap: () {
-                          // url launcher
-                        },
-                        child: Row(
-                          children: [
-                            const Icon(Icons.favorite),
-                            Text(' $likeCount'),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      child: Text(' ${difference.inHours.toString()}時間前'),
+                      child: difference.inHours == 0
+                          ? Text('${difference.inMinutes.toString()}分前')
+                          : Text('${difference.inHours.toString()}時間前'),
                     )
                   ],
                 ),
